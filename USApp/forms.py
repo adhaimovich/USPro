bb7fc868f7d887d00d01e541ca76638e144707e2
ADHaimovich <adhaimovich@users.noreply.github.com>
2018-03-05 17:24:32 -0500

    Add files via upload

--- USApp/forms.py
+++ USApp/forms.py
@@ -8,53 +8,45 @@ from django import forms
 # Form for the avi inputs
 class AVIForm(forms.Form):
     #Core Features
-    dcm_compressed = forms.BooleanField(initial=True, required=False)
-    anonymize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'aviAnonymizeOptions()'}))
-    image_thresholding = forms.ChoiceField(choices=[('1', 'binary'), ('2', 'Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
-    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
-    crop = forms.BooleanField(initial=True, required=False)
+    anonymize = forms.BooleanField(initial=True, required=False)
+    crop = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'aviCropOptions()'}))
     resize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'aviResizeOptions()'}))
     r_dim = forms.IntegerField(initial=400, required=True, help_text='r_dim_tr')  # only required if resize == True
     c_dim = forms.IntegerField(initial=608, required=True, help_text='c_dim_tr')  # only required if resize == True
-    rgb = forms.BooleanField(initial=False, required=False)
     output_type = forms.ChoiceField(choices=[('1', 'avi'), ('2', 'mpeg')], initial='.avi', required=False)
     #Advanced Features
     image_increment = forms.IntegerField(initial=10, required=False)
-    fps = forms.IntegerField(initial=10, required=False)
-    gamma = forms.FloatField(initial=4, required=False, help_text='gamma_tr')
     verbose = forms.BooleanField(initial=False, required=False)
+    image_thresholding = forms.ChoiceField(choices=[('1', 'binary'), ('2', 'Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
+    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
 
 # Form for the pickle inputs
 class PickleForm(forms.Form):
     #Core features
-    dcm_compressed = forms.BooleanField(initial=True, required=False)
-    anonymize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'pickleAnonymizeOptions()'}))
-    image_thresholding = forms.ChoiceField(choices=[('1','binary'), ('2','Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
-    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
-    crop = forms.BooleanField(initial=True, required=False)
+    anonymize = forms.BooleanField(initial=True, required=False)
+    crop = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'pickleCropOptions()'}))
     resize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'pickleResizeOptions()'}))
     r_dim = forms.IntegerField(initial=240, required=False, help_text='r_dim_tr')
     c_dim = forms.IntegerField(initial=320, required=False, help_text='c_dim_tr')
-    rgb = forms.BooleanField(initial=True, required=False)
     pickle_file = forms.CharField(initial='out.p', required=False)
     #Advanced features
     image_increment = forms.IntegerField(initial=10, required=False)
     verbose = forms.BooleanField(initial=False, required=False)
+    image_thresholding = forms.ChoiceField(choices=[('1','binary'), ('2','Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
+    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
 
 # Form for the HDF5 inputs
 class HDF5Form(forms.Form):
     #Core features
-    dcm_compressed = forms.BooleanField(initial=True, required=False)
-    anonymize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'hdf5AnonymizeOptions()'}))
-    image_thresholding = forms.ChoiceField(choices=[('1','binary'), ('2','Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
-    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
-    crop = forms.BooleanField(initial=True, required=False)
+    anonymize = forms.BooleanField(initial=True, required=False)
+    crop = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'hdf5CropOptions()'}))
     resize = forms.BooleanField(initial=True, required=False, widget=forms.CheckboxInput(attrs={'onclick': 'hdf5ResizeOptions()'}))
     r_dim = forms.IntegerField(initial=240, required=False, help_text='r_dim_tr')
     c_dim = forms.IntegerField(initial=320, required=False, help_text='c_dim_tr')
-    rgb = forms.BooleanField(initial=True, required=False)
     h5_file = forms.CharField(initial='out.hdf', required=False)
     #Advanced features
+    image_thresholding = forms.ChoiceField(choices=[('1','binary'), ('2','Otsu')], initial='binary', required=False, help_text='image_thresholding_tr')
+    cleaning_threshold = forms.FloatField(initial=.0000001, required=False, help_text='cleaning_threshold_tr')
     image_increment = forms.IntegerField(initial=10, required=False)
     compression = forms.ChoiceField(choices=[('1','gzip'), ('2','lzf')], initial='gzip', required=False)
     verbose = forms.BooleanField(initial=True, required=False)
